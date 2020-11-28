3444fc69e6cd7cdc4c3164cb7d5126755bfbdaea
Nehlk <slek.dev@outlook.com>
2020-11-28 14:38:47 -0300

    as

--- src/Middlewares/AuthMiddleware.php
+++ src/Middlewares/AuthMiddleware.php
@@ -33,12 +33,14 @@ class AuthMiddleware
         $headers = getallheaders();
         $token = $headers['token'];
 
-        $jwt = JWT::decode($token, KEY2, array('HS256'));
-        echo json_encode($jwt);
+        
+        
         $resp = new stdClass;
         $resp->date = date('Y-m-d');
 
         try {
+            $jwt = JWT::decode($token, KEY2, array('HS256'));
+            
             if ($jwt->type == $this->_typeuser) {
 
                 $response = $handler->handle($request);
