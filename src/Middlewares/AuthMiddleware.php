485abb6757b04e082567397d5586fc9479b9a0b8
Nehlk <slek.dev@outlook.com>
2020-11-28 15:10:50 -0300

    a

--- src/Middlewares/AuthMiddleware.php
+++ src/Middlewares/AuthMiddleware.php
@@ -30,8 +30,8 @@ class AuthMiddleware
     public function __invoke(Request $request, RequestHandler $handler): Response
     {
 
-        $headers = getallheaders();
-        $token = $headers['token'];
+        // $headers = getallheaders();
+        $token = $request->headers->get('token');
 
         $res = new stdClass;
         $res->date = date('Y-m-d');
@@ -40,22 +40,22 @@ class AuthMiddleware
         try {
             echo json_encode($token);
             $jwt = JWT::decode($token, KEY2, array('HS256'));
-            echo 'after JWT';
+            
             if ($jwt->type == $this->_typeuser) {
-                echo '1st IF';
+                
                 $response = $handler->handle($request);
                 $existingContent = (string) $response->getBody();
-                echo 'exists content';
+                
                 $resp->getBody()->write($existingContent);
-                echo 'resp 1';
+               
                 return $resp;
             } else {
                 $res->message = "Tipo de Usuario de Usuario Invalido";
-                echo 'else';
+                
                 $resp->getBody()->write(json_encode($res));
             }
         } catch (\Throwable $th) {
-            echo 'th';
+            
             $res->message = $th->getMessage();
             $resp->getBody()->write(json_encode($res));
         } 
